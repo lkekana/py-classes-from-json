4063a4c430f7a10a21abe7c4f25d76346e5753d9
lkekana <theoriginalles@gmail.com>
2023-11-30 21:45:34 +0200

    big functionality improvements wow
    - removed comments for self-explanatory lines
    - added sanitize_input for removing invalid chars
    - added ensure_proper_py_names
    - more polite in asking for input
    - added module_name so jschema_to_python would work properly
    - removed extra whitespace in prints
    - changed output directory to module name now instead of class name

--- make.py
+++ make.py
@@ -7,48 +7,56 @@ import json
 from genson import SchemaBuilder
 import subprocess
 from pathlib import Path
+import re
 
-# Path to the directory containing this script.
 SCRIPT_DIR = Path(__file__).parent
-
-# Path to the directory containing the JSON files.
 INPUT_DIR = SCRIPT_DIR / 'input'
-
-# Path to the directory containing the generated Schema.
 SCHEMA_DIR = SCRIPT_DIR / 'schema'
-
-# Path to the directory containing output files.
 OUTPUT_DIR = SCRIPT_DIR / 'output'
 
+def sanitize_input(input_str):
+    # Replace invalid characters with underscores
+    return re.sub(r'[^A-Za-z0-9_]', '_', input_str)
+
+def ensure_proper_py_names(input_str):
+    # Prepend an underscore if the string starts with a number
+    if re.match(r'^[0-9]', input_str):
+        input_str = '_' + input_str
+
+    # Replace invalid characters with underscores
+    input_str = sanitize_input(input_str)
+
+    # Prevent starting with an underscore
+    if input_str.startswith('_'):
+        input_str = 'x' + input_str
+    return input_str
+
 def main():
-    # Check that the input directory exists.
     if not INPUT_DIR.exists():
         print(f'Input directory does not exist: {INPUT_DIR}')
         return
     
-    # Create the output directory if it doesn't exist.
     if not OUTPUT_DIR.exists():
         print(f'Creating output directory: {OUTPUT_DIR}')
         OUTPUT_DIR.mkdir()
     
-    # Check if there are any JSON files in the input directory.
     if not any(INPUT_DIR.glob('*.json')):
         print(f'No JSON files found in input directory: {INPUT_DIR}')
         return
     
-    # Get name for class at root of object model.
-    root_class_name = input('Enter name for root class: ')
+    root_class_name = input('Please enter a name for root class: ')
+    root_class_name = sanitize_input(root_class_name)
+    module_name = input('Please enter a name for Python module: ')
+    module_name = sanitize_input(module_name)
 
-    PY_DIR = OUTPUT_DIR / root_class_name.lower()
+    PY_DIR = OUTPUT_DIR / module_name.lower()
 
-    # Delete the output directory if it exists.
     if PY_DIR.exists():
         print(f'Deleting existing output directory: {PY_DIR}')
         for file in PY_DIR.glob('*'):
             os.remove(file)
         os.rmdir(PY_DIR)
 
-    # Create a schema from the JSON files.
     print('Reading JSON files...')
     builder = SchemaBuilder()
     for file in INPUT_DIR.glob('*.json'):
@@ -56,19 +64,15 @@ def main():
         with open(file) as f:
             builder.add_object(json.load(f))
 
-    # Add the root class name to the schema.
     JSON_SCHEMA = builder.to_schema()
     JSON_SCHEMA['title'] = root_class_name
 
-    print('\nGenerating schema...')
-    # Write the schema to a file.
+    print('Generating schema...')
     schema_file = OUTPUT_DIR / f'{root_class_name.lower()}.json'
     with open(schema_file, 'w') as f:
         f.write(json.dumps(JSON_SCHEMA, indent=2))
 
-
-    print('\nGenerating Python class...')
-    # Generate a Python class from the schema.
+    print('Generating Python class...')
     subprocess.run([
         sys.executable,
         '-m',
@@ -79,15 +83,15 @@ def main():
         str(PY_DIR),
         '-r',
         root_class_name,
-        '-v'
+        '-m',
+        module_name,
     ], check=True)
 
     # Re-write the JSON schema, because jschema_to_python deletes it.
     with open(schema_file, 'w') as f:
         f.write(builder.to_json(indent=2))
 
-    
-    print('\nDone.')
+    print('Done.')
 
 
 if __name__ == '__main__':
